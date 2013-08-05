4a18815ed8df9b717c4399c9197c289512737af6
Gleb Teterin <gleb.teterin@gmail.com>
2013-08-05 23:13:44 +1200

    chore: add GetCardReadersList method

---
+++ src/Lando/LowLevel/OperationResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Lando.LowLevel
+{
+	public class OperationResult
+	{
+		public bool IsSuccessful { get; private set; }
+
+		public int StatusCode { get; private set; }
+
+		public string StatusName { get; set; }
+
+		public string StatusDescription { get; private set; }
+
+		public OperationResult(bool isSuccessful, int statusCode, string statusName, string statusDescription)
+		{
+			IsSuccessful = isSuccessful;
+			StatusCode = statusCode;
+			StatusName = statusName;
+			StatusDescription = statusDescription;
+		}
+	}
+}
