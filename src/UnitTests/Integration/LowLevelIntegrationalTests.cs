58e86e10cc89684a97ddead5e1194c591d6d35ad
Gleb Teterin <gleb.teterin@gmail.com>
2013-08-05 23:32:18 +1200

    test: add tests for cardreader name

---
+++ src/UnitTests/Integration/LowLevelIntegrationalTests.cs
@@ -0,0 +1,50 @@
+using System;
+using Lando.LowLevel;
+using NUnit.Framework;
+
+namespace Lando.UnitTests.Integration
+{
+	[TestFixture]
+	[Explicit]
+	public class LowLevelIntegrationalTests
+	{
+		readonly LowLevelCardReader _reader = new LowLevelCardReader();
+
+		private bool _isContextEstablished;
+
+		[SetUp]
+		public void Setup()
+		{
+			var fact = _reader.EstablishContext();
+
+			_isContextEstablished = fact == 0;
+
+			if(!_isContextEstablished)
+				throw new Exception("Context is not established");
+		}
+
+		[Test]
+		public void EstablishContext()
+		{
+			Assert.That(_isContextEstablished, Is.True);
+		}
+
+		[Test]
+		public void GetCardReadersList()
+		{
+			string[] readerNames;
+
+			// act
+			var getCardReadersResult = GetCardreaderNames(out readerNames);
+
+			Assert.That(getCardReadersResult, Is.True);
+			Assert.That(readerNames.Length, Is.EqualTo(1));
+		}
+
+		private bool GetCardreaderNames(out string[] result)
+		{
+			var getCardReadersResult = _reader.GetCardReadersList(out result);
+			return getCardReadersResult.IsSuccessful;
+		}
+	}
+}
