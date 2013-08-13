278091de4dd2d2ff7fc61170ff5878ee6d5bbab1
Gleb Teterin <gleb.teterin@gmail.com>
2013-08-13 22:08:05 +1200

    chore: add ubuntu/mono support

--- src/Lando/LowLevel/CardState.cs
+++ src/Lando/LowLevel/CardState.cs
@@ -1,3 +1,4 @@
+using System;
 using Lando.LowLevel.Enums;
 
 namespace Lando.LowLevel
@@ -28,6 +29,17 @@ namespace Lando.LowLevel
 				case WinscardWrapper.SCARD_POWERED: resultStateType = CardStateType.CardPowered; break;
 				case WinscardWrapper.SCARD_NEGOTIABLE: resultStateType = CardStateType.CardNegotiable; break;
 				case WinscardWrapper.SCARD_SPECIFIC: resultStateType = CardStateType.CardSpecific; break;
+			}
+
+			// on ubuntu the result of SCardStatus for presented card is newer equals 2 and always different
+			// so I consider any unexpected value as CardPresent except 1-999 - it's a system error in Windows
+
+			if (resultStateType == CardStateType.None && cardState > 999)
+			{
+				if (cardState > 999)
+					resultStateType = CardStateType.CardPresent;
+				else
+					throw new Exception("Supplied parameter is out of scope of WinscardWrapper.");
 			}
 
 			return resultStateType;
