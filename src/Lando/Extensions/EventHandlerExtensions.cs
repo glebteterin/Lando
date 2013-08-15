693e3ebbbac307e5c745c468026cd8b2bc58cf42
Gleb Teterin <gleb.teterin@doneright.ru>
2013-08-15 23:12:01 +1200

    fix: use safe event raising

---
+++ src/Lando/Extensions/EventHandlerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lando.Extensions
+{
+	public static class EventHandlerExtensions
+	{
+		public static void SafeInvoke(this CardreaderEventHandler evt, object sender, CardreaderEventArgs e)
+		{
+			if (evt != null)
+			{
+				evt(sender, e);
+			}
+		}
+	}
+}
