ce2c5df34093b1692b74fbfd681ddeeb2c3be098
Gleb Teterin <gleb.teterin@gmail.com>
2013-08-25 19:22:08 +1200

    chore: update AssemblyInfo

--- src/Lando/Properties/AssemblyInfo.cs
+++ src/Lando/Properties/AssemblyInfo.cs
@@ -1,38 +1,27 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Lando")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Lando")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+#if NET4_0
+	[assembly: AssemblyTitle("Lando for .NET Framework 4")]
+#elif NET4_5
+	[assembly: AssemblyTitle("Lando for .NET Framework 4.5")]
+#endif
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyDescription("Lando")]
+[assembly: AssemblyCompany("Lando")]
+[assembly: AssemblyProduct("Lando v1.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyCopyright("Copyright © 2013 by Gleb Teterin")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4905c28f-ec22-4053-bcb9-87d388434a26")]
-
 [assembly: InternalsVisibleTo("Lando.UnitTests")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
