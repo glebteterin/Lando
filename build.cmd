1d13362f4bff42871b14c1c00e5467c4daee2123
Gleb Teterin <gleb.teterin@gmail.com>
2013-08-25 18:49:27 +1200

    chore: add configuration parameter to build script

--- build.cmd
+++ build.cmd
@@ -1,8 +1,26 @@
 @echo off
 set MSBUILD=%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe
 set MSBUILD_SCRIPT="%~dp0src\Lando.proj"
+
+set MSBUILD_ARGUMENTS=
+:next
+if (%1)==() goto build
+if (%1)==(debug) (
+	set MSBUILD_ARGUMENTS=%MSBUILD_ARGUMENTS% /p:Configuration=Debug
+	shift
+	goto next
+)
+
+if (%1)==(release) (
+	set MSBUILD_ARGUMENTS=%MSBUILD_ARGUMENTS% /p:Configuration=Release
+	shift
+	goto next
+)
+
+
 :build
 echo MSBUILD: %MSBUILD%
 echo MSBUILD_SCRIPT: %MSBUILD_SCRIPT%
-%MSBUILD% /nologo /fl %MSBUILD_SCRIPT%
+echo MSBUILD_ARGUMENTS: %MSBUILD_ARGUMENTS%
+%MSBUILD% /nologo /fl %MSBUILD_SCRIPT% %MSBUILD_ARGUMENTS%
 exit /b 0
